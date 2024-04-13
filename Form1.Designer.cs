32d65df0e8b71ecd2110b0e334916caae0753e13
AasthaPatni <aasthapatni90@gmail.com>
2024-04-13 16:35:48 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DiceRoller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Playbill", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.label1.Location = new System.Drawing.Point(126, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(533, 62);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WELCOME TO DICE ROLLER";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(184, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 147);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(422, 116);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(153, 147);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button1.Font = new System.Drawing.Font("Playbill", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(184, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(391, 56);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ROLL";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 442);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Dice Roller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiceRoller
+{
+    public partial class Form1 : Form
+    {
+        Random dice = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int randomNum1 = dice.Next(1, 7);
+            int randomNum2 = dice.Next(1, 7);
+            switch (randomNum1)
+            {
+                case 1:
+                    pictureBox1.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-one (1).png";
+                    pictureBox1.SizeMode  = PictureBoxSizeMode.StretchImage; 
+                    break;
+                 case 2: pictureBox1.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-two.png";
+                    pictureBox1.SizeMode =(PictureBoxSizeMode.StretchImage);
+                    break;
+                    case 3:
+                    pictureBox1.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-three.png";
+                    pictureBox1.SizeMode = (PictureBoxSizeMode.StretchImage);
+                    break;
+                    case 4:
+                    pictureBox1.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-four.png";
+                    pictureBox1.SizeMode = (PictureBoxSizeMode.StretchImage);
+                    break;
+                    case 5:
+                    pictureBox1.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-five.png";
+                    pictureBox1.SizeMode = (PictureBoxSizeMode.StretchImage);
+                    break;
+                    case 6:
+                    pictureBox1.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-six.png";
+                    pictureBox1.SizeMode = (PictureBoxSizeMode.StretchImage);
+                    break;
+                default:
+                    pictureBox1.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-one (1).png";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+
+
+                      
+            }
+            switch (randomNum2)
+            {
+                case 1:
+                    pictureBox2.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-one (1).png";
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 2:
+                    pictureBox2.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-two.png";
+                    pictureBox2.SizeMode = (PictureBoxSizeMode.StretchImage);
+                    break;
+                case 3:
+                    pictureBox2.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-three.png";
+                    pictureBox2.SizeMode = (PictureBoxSizeMode.StretchImage);
+                    break;
+                case 4:
+                    pictureBox2.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-four.png";
+                    pictureBox2.SizeMode = (PictureBoxSizeMode.StretchImage);
+                    break;
+                case 5:
+                    pictureBox2.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-five.png";
+                    pictureBox2.SizeMode = (PictureBoxSizeMode.StretchImage);
+                    break;
+                case 6:
+                    pictureBox2.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-six.png";
+                    pictureBox2.SizeMode = (PictureBoxSizeMode.StretchImage);
+                    break;
+                default:
+                    pictureBox2.ImageLocation = @"C:\Users\apoor\Downloads\dice-six-faces-one (1).png";
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+            }
+            }
+        }
+}
